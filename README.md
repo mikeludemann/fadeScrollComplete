a5c7498cd17d6e291b13a7769a47b649554aa533
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-09 16:17:02 +0100

    First data with all informations

---
+++ README.md
@@ -0,0 +1,11 @@
+# fadeScrollComplete
+
+FadeIn and FadeOut on scroll with select element(s)
+
+Example
+```code
+fadeScrollComplete(Elements)
+
+Testing with Parameter
+fadeScrollComplete(".fade");
+```
---
+++ fadeScrollComplete.js
@@ -0,0 +1,29 @@
+function fadeScrollComplete(elements){
+    
+  $(window).on("load",function() {
+  
+    $(window).scroll(function() {
+    
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      
+      $(elements).each(function() {
+      
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+        if (objectBottom < windowBottom) { 
+        
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+          
+        } else { 
+        
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+          
+        }
+        
+      });
+      
+    }).scroll();
+    
+  });
+  
+}
